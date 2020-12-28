a3b032c15de4499ccadc4b49c047463f0e76cdcd
Emma Mendez <emmamendez07@gmail.com>
2020-12-28 19:09:45 +0000

    working code and functions- all sound, error fixed.

--- server.js
+++ server.js
@@ -27,19 +27,23 @@
 //         message: 'Signing Up'
 // })
 // })
-
+if (process.env.NODE_ENV !== 'productiom') {
+    require('dotenv').config()
+}
 
 const express = require('express')
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
 
-// const initializePassport = require('./passport-config')
-// initializePassport(
-//     passport, 
-//     email => users.find(user => user.email === email)
-//     )
-    // 
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport, 
+    email => users.find(user => user.email === email)
+    )
+    
 
 
 const users = []
@@ -47,6 +51,14 @@ const users = []
 
 app.set('view-engine', 'ejs')
 app.use(express.urlencoded({extended: false}))
+app.use(flash());
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 
 app.get('/', (req, res) => {
@@ -58,9 +70,11 @@ app.get('/login', (req, res) => {
 
 })
 
-app.post('/login', (req, res) => {
-    
-})
+app.post('/login', passport.authenticate('local', {
+    successRedirect: '/',
+    failureFlash: true
+}))
+
 
 app.get('/register', (req, res) => {
     res.render('register.ejs')
