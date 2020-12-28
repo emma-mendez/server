e528c61650f4220e6146f812f5ac4a04043aadb8
Emma Mendez <emmamendez07@gmail.com>
2020-12-28 15:07:06 +0000

    working code, I have a problem with the passport and initialize, code is commented out code has developed from last commit

--- server.js
+++ server.js
@@ -31,8 +31,23 @@
 
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+
+// const initializePassport = require('./passport-config')
+// initializePassport(
+//     passport, 
+//     email => users.find(user => user.email === email)
+//     )
+    // 
+
+
+const users = []
+
 
 app.set('view-engine', 'ejs')
+app.use(express.urlencoded({extended: false}))
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs', { name: 'Emma'})
@@ -43,13 +58,32 @@ app.get('/login', (req, res) => {
 
 })
 
+app.post('/login', (req, res) => {
+    
+})
+
 app.get('/register', (req, res) => {
     res.render('register.ejs')
 
 })
 
-app.post('/register', (req, res) => {
-    
+app.post('/register', async (req, res) => {
+    try {
+        // this will be auto generated when linked to Mongo, temporary for now!
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        // if failed redirects to register!
+        res.redirect('/register')
+
+    }
+console.log(users)   
 })
 // start the server
 app.listen(1000, () => {console.log('Emmas Server Up & Running')})
