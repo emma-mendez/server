52e3a012fbac639db8a131a0b376147d8094970d
Emma Mendez <emmamendez07@gmail.com>
2021-01-01 23:55:44 +0000

    small change to server, commented out some stuff server working

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ if (process.env.NODE_ENV !== 'productiom') {
 }
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const bcrypt = require('bcrypt')
 const passport = require('passport')
@@ -10,7 +11,8 @@ const flash = require('express-flash')
 const session = require('express-session')
 const methodOverride = require('method-Override')
 
-
+app.use(cors())
+app.use(express.json())
 
 
 const initializePassport = require('./passport-config')
@@ -37,49 +39,77 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(methodOverride('_method'))
 
+
+// EXAMPLE JSON ROUTE
+app.get('/register', (req, res) => {
+    res.json({
+        message: 'You got this - this is some server data',
+        areYouLoggedIn: true 
+    })
+})
+
 // RENDER INDEX PAGE
 
-app.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', { name: req.user.name })
-})
+// app.get('/', checkAuthenticated, (req, res) => {
+//     res.render('index.ejs', { name: req.user.name })
+// })
 
-//  GET & POST LOGIN
-app.get('/login', checkNotAuthenticated, (req, res) => {
-    res.render('login.ejs')
+// //  GET & POST LOGIN
+// app.get('/login', checkNotAuthenticated, (req, res) => {
+//     res.render('login.ejs')
 
-})
+// })
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
-    successRedirect: '/',
-    failureFlash: true
-}))
+// app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
+//     successRedirect: '/',
+//     failureFlash: true
+// }))
 
 
 //  GET & POST REGISTRATION
 
-app.get('/register', checkNotAuthenticated, (req, res) => {
-    res.render('register.ejs')
-
+app.get('/signup', (req, res) => {
+    res.json({
+        message: "Signup request received and done!"
+    })
 })
 
-app.post('/register', checkNotAuthenticated, async (req, res) => {
-    try {
-        // this will be auto generated when linked to Mongo, temporary for now!
-        const hashedPassword = await bcrypt.hash(req.body.password, 10)
-        users.push({
-            id: Date.now().toString(),
-            name: req.body.name,
-            email: req.body.email,
-            password: hashedPassword
-        })
-        res.redirect('/login')
-    } catch {
-        // if failed redirects to register!
-        res.redirect('/register')
-
-    }
-console.log(users)   
+app.post('/example-one', (req, res) => {
+    console.log(req.body)
+    res.json({
+        message: `Example 1 is no fun, but your greeting was ${req.body.greeting}`,
+    })
 })
+
+app.get('/example-two', (req, res) => {
+    res.json({
+        message: "Example 2 leaves me blue"
+    })
+})
+
+// app.get('/register', checkNotAuthenticated, (req, res) => {
+//     res.render('register.ejs')
+
+// })
+
+// app.post('/register', checkNotAuthenticated, async (req, res) => {
+//     try {
+//         // this will be auto generated when linked to Mongo, temporary for now!
+//         const hashedPassword = await bcrypt.hash(req.body.password, 10)
+//         users.push({
+//             id: Date.now().toString(),
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: hashedPassword
+//         })
+//         res.redirect('/login')
+//     } catch {
+//         // if failed redirects to register!
+//         res.redirect('/register')
+
+//     }
+// console.log(users)   
+// })
 
 
 // LOG OUT FEATURE
